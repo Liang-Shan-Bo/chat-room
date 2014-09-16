5e2295a3c051e0c6c5e8dd3cdf72d3acdd80a6a8
yehan432 <yh432@sina.com>
2014-09-16 17:21:06 +0800

    Signed-off-by: yehan432 <yh432@sina.com>

---
+++ src/main/java/com/lsb/cr/action/RestController.java
@@ -0,0 +1,29 @@
+package com.lsb.cr.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RestController {
+	public RestController() {
+	}
+
+	@RequestMapping(value = "/login/{user}", method = RequestMethod.GET)
+	public ModelAndView myMethod(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable("user") String user,
+			ModelMap modelMap) throws Exception {
+		modelMap.put("loginUser", user);
+		return new ModelAndView("/login/login", modelMap);
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String registPost() {
+		return "/login";
+	}
+}
