5100c4f2c1c5fc9a03b183170ea4c0cb1dd62695
yehan432 <yh432@sina.com>
2014-09-29 16:45:34 +0800

    Signed-off-by: yehan432 <yh432@sina.com>

--- src/main/java/com/lsb/cr/channel/action/ChannelAction.java
+++ src/main/java/com/lsb/cr/channel/action/ChannelAction.java
@@ -3,24 +3,25 @@ package com.lsb.cr.channel.action;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.lsb.cr.channel.service.ChannelService;
+import com.lsb.cr.core.CRConstants;
 
+
+@Secured(CRConstants.ROLE.ROLE_USER)
 @Controller
 public class ChannelAction {
-	
+
 	@Autowired
 	private ChannelService channelService;
 
 	@RequestMapping(value = { "/channel" }, method = RequestMethod.GET)
-	public String showChannelPage(Principal principal,Model model) {
-		if (principal == null) {
-			return "redirect:/top";
-		}
+	public String showChannelPage(Principal principal, Model model) {
 		model.addAttribute("ChannelList", channelService.getChannelList());
 		return "channel/channel";
 	}
